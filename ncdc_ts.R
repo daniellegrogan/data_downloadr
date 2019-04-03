cf9442359c55a03a7e9cfda1dd6c1e098cddbddd
Danielle Grogan <dgrogan@yukon.sr.unh.edu>
2019-04-03 09:20:59 -0400

    added ncdc_ts.R

---
+++ ncdc_ts.R
@@ -0,0 +1,50 @@
+# ncdc_ts()
+
+# Download and save a time series of NCDC data using the rnoaa package (https://github.com/ropensci/rnoaa)
+# Danielle S Grogan
+# last updated: 2019-04-01
+
+library(rnoaa)
+
+ncdc_ts = function(datasetid,       # character string; example: "GHCN". Use ncdc_datasets() to get a table of all dataset options 
+                   stationid,       # character string; example: "GHCND:US1MDSM0001". Use ncdc_stations() to find station ids
+                   datatypeid,      # character string; example: "PRCP".  Use ncdc_datatypes() to find options
+                   yrs,             # vector of integers. A sequence of years for which to download data.
+                   token,           # This must be a valid token token supplied to you by NCDC's Climate Data Online access token generator (http://www.ncdc.noaa.gov/cdo-web/token). 
+                   out.nm = NA,     # character sting; if provided, the data is written to this file name.
+                   write.empty = F  # T or F;  if T then empty data frames will be saved to file (not recommended).
+                   )
+  { 
+  
+  st.data.all  = lapply(yrs, 
+                        FUN = function(x) ncdc(datasetid  = datasetid, 
+                                               stationid  = stationid,
+                                               datatypeid = datatypeid, 
+                                               startdate  = paste(x, '-01-01', sep=""), 
+                                               enddate    = paste(x, '-12-31', sep=""), 
+                                               limit      = 365,
+                                               token      = token)
+  )
+  
+  st.data.list = lapply(st.data.all, FUN = function(x) x$data)
+  st.data.df = do.call(rbind, st.data.list)
+  
+  if(is.na(out.nm)){
+    out = st.data.df
+  }else{
+    if(length(st.data.df) > 0){
+      write.table(st.data.df, out.nm, row.names=F, sep=",")
+      out = paste("data written to", out.nm)
+    }else{
+      if(write.empty == T){
+        write.table(st.data.df, out.nm, row.names=F, sep=",")
+        out = paste("Empty data frame written to", out.nm)
+      }else{
+        out = "Empty data frame. No file written."
+      }
+    }
+    
+  }
+  return(out)
+}
+# END OF FUNCTION
